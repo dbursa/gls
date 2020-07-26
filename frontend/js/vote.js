98701428a52228d8fa73d9c609f848f4fb37bd7c
Vojtech <Rerabek>
2020-07-26 22:13:31 +0200

    first load request bug fix

--- frontend/js/vote.js
+++ frontend/js/vote.js
@@ -17,6 +17,55 @@ button_close.classList.add("d-none")
 
 
 let voteProject = null
+
+// REQUEST
+function vote(){
+  // Pokud neni voteProject variable nastavena (refresh s otevrenym popup)
+  if (voteProject === null){
+    window.location.href = ""
+    return;
+  }
+
+  // Clear alerts
+  popup_error.classList.add("d-none")
+  popup_success.classList.add("d-none")
+
+  var email = email_input.value
+
+  // PARAMS
+  const params = new URLSearchParams()
+  params.append('csrf_name', csrf_name_val)
+  params.append('csrf_value', csrf_value_val)
+  params.append('project_id', voteProject)
+  params.append('email', email)
+
+  console.log("VOTING FOR " + voteProject + "using email:" + email)
+
+
+  const config = {
+  headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+  }}
+
+  axios.post('/api/vote', params, config)
+  .then(res => {
+    // ZDE RESIT ZE SUCCESSFULL REQUEST
+    popup_success.classList.add("d-block")
+    button_close.classList.add("d-inline-block")
+    popup_error.classList.remove("d-block")
+    popup_error.classList.add("d-none")
+    email_input.classList.add("d-none")
+    button_vote.classList.add("d-none")
+    popup_text.classList.add("d-none")
+
+  })
+  .catch(err => {
+    popup_error.classList.add("d-block")
+    popup_error.innerHTML = err.response.data.status
+    
+  })
+}
+
 // EVENT LISTENERS
 document.getElementById("vote_1").addEventListener("click", () => {voteProject = 1})
 document.getElementById("vote_2").addEventListener("click", () => {voteProject = 2})
@@ -25,50 +74,3 @@ document.getElementById("vote_3").addEventListener("click", () => {voteProject =
 document.getElementById("button_vote").addEventListener("click", () => vote())
 
 
-// REQUEST
-function vote(){
-    // Pokud neni voteProject variable nastavena (refresh s otevrenym popup)
-    if (voteProject === null){
-      window.location.href = ""
-      return;
-    }
-
-    // Clear alerts
-    popup_error.classList.add("d-none")
-    popup_success.classList.add("d-none")
-
-    var email = email_input.value
-
-    // PARAMS
-    const params = new URLSearchParams()
-    params.append('csrf_name', csrf_name_val)
-    params.append('csrf_value', csrf_value_val)
-    params.append('project_id', voteProject)
-    params.append('email', email)
-
-    console.log("VOTING FOR " + voteProject + "using email:" + email)
-
-
-    const config = {
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }}
-
-    axios.post('/api/vote', params, config)
-    .then(function (response) {
-      // ZDE RESIT ZE SUCCESSFULL REQUEST
-      popup_success.classList.add("d-block")
-      button_close.classList.add("d-inline-block")
-      popup_error.classList.remove("d-block")
-      popup_error.classList.add("d-none")
-      email_input.classList.add("d-none")
-      button_vote.classList.add("d-none")
-      popup_text.classList.add("d-none")
-
-    })
-    .catch(err => {
-      popup_error.classList.add("d-block")
-      popup_error.innerHTML = err.response.data.status
-      
-    })
-}
--- frontend/main.php
+++ frontend/main.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="frontend/css/style.css">
 
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <meta name="csrf_name" content="<?= $name ?>" />
     <meta name="csrf_value" content="<?= $value ?>" />
     
@@ -209,17 +208,15 @@
                 Váš hlas byl odeslán!
             </div>
             <div class="col-12 no-padding">
-                <form>
                     <div class="col-12 no-padding pt-3 pb-4">
                         <input class="w-100 p-1" type="email" id="email_input">
                     </div>
                     <div class="col-12 no-padding text-center">
-                        <button class="px-4 py-3" type="submit" id="button_vote"><b>Odeslat hlas</b></button>
+                        <button class="px-4 py-3" id="button_vote"><b>Odeslat hlas</b></button>
                     </div>
                     <div class="col-12 no-padding text-center">
                         <button class="px-4 py-3 vote" id="button_close"><b><a href="#">Zavřít</a></b></button>
                     </div>
-                </form>
             </div>
         </div>
     </div>
@@ -446,6 +443,7 @@
         </div>
     </footer>
 </section>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
